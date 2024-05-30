1c73037630e6414e4e59bf89db7f73f9929bffd6
Temesgen dora <tdemeke36@gmail.com>
2024-05-30 13:43:02 -0700

    done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,24 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+
 def pascal_triangle(n):
-  if n <= 0:
+   """_summary_
+
+    Args:
+        n (_type_): _description_
+
+    Returns:
+        _type_: _description_
+   """
+   if n <= 0:
     return []
-  result = [[1]]
-  for i in range(1, n):
-    row = [1]
-    for j in range(1, i):
-      row.append(result[i-1][j-1] + result[i-1][j])
-    row.append(1)
-    result.append(row)
-  return result
+   result = [[1]]
+   for i in range(1, n):
+       row = [1]
+       for j in range(1, i):
+           row.append(result[i-1][j-1] + result[i-1][j])
+       row.append(1)
+       result.append(row)
+   return result
