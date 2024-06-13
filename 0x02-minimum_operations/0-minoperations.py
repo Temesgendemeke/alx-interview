e65de78c72b58c47f7fc81293aa13d8ccbe3c34a
Temesgen dora <tdemeke36@gmail.com>
2024-06-13 12:22:10 -0700

    done task 2

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Solution to minimum operations interview practice question. See README.md
+"""
+
+
+from sys import argv
+
+
+def minOperations(n):
+    """
+    Calculates the minimum number of operations to go from one 'H' to n 'H's
+    """
+
+    if n <= 1:
+        return 0
+
+    for i in range(2, int((n/2)+1)):
+        if n % i == 0:
+            return minOperations(int(n / i)) + i
+
+    return n
